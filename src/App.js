08997720bcc72d3ce2ac7c09ce21b75c686da70e
patrickbj <patrick.brison@gmail.com>
2020-07-13 19:35:12 -0300

    Animation e custom scroll bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-// import { BrowserRouter, Switch, useHistory, Route, Link } from 'react-router-dom'
+import React, {useState, useEffect} from 'react';
 import { Switch, Route, Link } from 'react-router-dom'
 import './styles/styles.css';
 import logo from './assets/logo_imagination.png';
 import HomePage from './components/HomePage'
 import ViewImage from './components/ViewImage'
 
-
 function App() {
+
+  const [animacao, setAnimacao] = useState('animacao-inicial');
+  useEffect(() => {
+    setTimeout(() => { setAnimacao('animacao-hover') }, 2000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
   return (
     <div className="app">
       <div className="bar">
         <Link className="barImage" to={{pathname: '/'}}>
-          <img alt="logo imagination" className="logo" src={logo} draggable="false"/>
+          <img alt="logo imagination" className={`logo ${animacao}`} src={logo} draggable="false"/>
         </Link>
       </div>
       <div className="content">
