c6942eff630beead928037a89bfde0ff90be7dab
patrickbj <patrick.brison@gmail.com>
2020-07-13 01:34:51 -0300

    Adicao todas as imagens

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -31,39 +31,43 @@ function HomePage(){
             <div className="progressCircle" hidden={!loading}>
                 <CircularProgress className="progressCircleChild" color="inherit" />
             </div>
-            {
-                listaImagem.map(
-                    (img, i) => {
-                        if(i<= 12){
-                            return(
-                                <div className={seletorColunaEOrdem(i+1)} key={i}>
-                                    {<Link to={{
-                                        pathname: '/ViewImage',
-                                        image:img}}>
-                                            {<img style={ajustaRatio(i+1, img)} alt={`${i}`} src={img.download_url} draggable="false"/>}
-                                    </Link>}
-                                </div>
-                            )}
-                    }
-                )
-            }
+            <div className="grid">
+                {
+                    listaImagem.map(
+                        (img, i) => {
+                            if(i<= 12){
+                                return(
+                                    <div className={seletorColunaEOrdem(i+1)} key={i}>
+                                        {<Link to={{
+                                            pathname: '/ViewImage',
+                                            image:img}}>
+                                                {<img style={ajustaRatio(i+1, img)} alt={`${i}`} src={img.download_url} draggable="false"/>}
+                                        </Link>}
+                                    </div>
+                                )}
+                        }
+                    )
+                }
+            </div>
 
-            {/* {
-                listaImagem.map(
-                    (img, i) => {
-                        if(i> 12){
-                            return(
-                                <div className={seletorColunaEOrdem(i+1)} key={i}>
-                                    {<Link to={{
-                                        pathname: '/ViewImage',
-                                        image:img}}>
-                                            {<img alt={`${i}`} src={img.download_url} draggable="false"/>}
-                                    </Link>}
-                                </div>
-                            )}
-                    }
-                )
-            } */}
+            <div className="others">
+                {
+                    listaImagem.map(
+                        (img, i) => {
+                            if(i> 12){
+                                return(
+                                    <div className="otherItem" key={i}>
+                                        {<Link to={{
+                                            pathname: '/ViewImage',
+                                            image:img}}>
+                                                {<img alt={`${i}`} src={img.download_url} draggable="false"/>}
+                                        </Link>}
+                                    </div>
+                                )}
+                        }
+                    )
+                }
+            </div>
         </div>
     );
 }
